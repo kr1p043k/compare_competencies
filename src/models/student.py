4beb4591941a9ab856d1e2a97a9dbb66afff6732
kr1p043k <opikmaks06@gmail.com>
2026-03-26 21:44:56 +0300

    переход с класса на модель

--- src/models/student.py
+++ src/models/student.py
@@ -1,9 +1,8 @@
-from dataclasses import dataclass
-from typing import List, Optional
+from pydantic import BaseModel, Field
+from typing import List, Dict
 
-@dataclass
-class Student:
-    id: str
-    name: str
-    skills: List[str]
-    target_profession: Optional[str] = None
+class StudentProfile(BaseModel):
+    student_id: str
+    name: str = "Unnamed Student"
+    competencies: List[str] = Field(default_factory=list)   # коды компетенций (SS1.1, DL-1.3 и т.д.)
+    target_role: str = "Data Scientist"                     # для будущей приоритизации
