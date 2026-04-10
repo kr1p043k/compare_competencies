ac0616c11c15c3620d4d0fc3df5197ceb7ccd8b3
kr1p043k <opikmaks06@gmail.com>
2026-04-11 01:09:06 +0300

    Обновлён __init__.py анализаторов: добавлены все публичные классы

--- src/analyzers/__init__.py
+++ src/analyzers/__init__.py
@@ -1,5 +1,15 @@
-# Маркер пакета
 from .comparator import CompetencyComparator
 from .gap_analyzer import GapAnalyzer
+from .skill_filter import SkillFilter
+from .profile_evaluator import ProfileEvaluator
+from .skill_level_analyzer import SkillLevelAnalyzer
+from .embedding_comparator import EmbeddingComparator
 
-__all__ = ['CompetencyComparator', 'GapAnalyzer']
+__all__ = [
+    'CompetencyComparator',
+    'GapAnalyzer',
+    'SkillFilter',
+    'ProfileEvaluator',
+    'SkillLevelAnalyzer',
+    'EmbeddingComparator',
+]
